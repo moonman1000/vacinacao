b360cb10d6f93b9098fc09b9f851f52a2a7a88d7
patricioborges <patricioborges.oficial@hotmail.com>
2025-06-22 18:28:02 -0300

    Atualiza arquivos do projeto

--- visualiza.php
+++ visualiza.php
@@ -2,7 +2,7 @@
 session_start();
 
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
-    header("location: visualiza.php");
+    header("location: login.php");
     exit;
 }
 ?>
